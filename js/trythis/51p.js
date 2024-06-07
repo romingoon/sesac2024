c74e9911e86dd87e3e5a48f5203ce297338b633e
romingoon <romingoon@gmail.com>
2024-06-07 16:42:09 +0900

    Trythis 52p - addPoints 함수 작성

---
+++ js/trythis/51p.js
@@ -0,0 +1,33 @@
+//오늘 날짜의 요일을 출력하는 switch문을 사용해서 작성해 보고, switch문을 사용하지 않은 더 간단한 방법도 찾아보세요.
+
+// switch문 사용
+const today = new Date();
+const day = today.getDay();
+
+switch (day) {
+  case 0:
+    console.log('오늘은 일요일입니다.');
+    break;
+  case 1:
+    console.log('오늘은 월요일입니다.');
+    break;
+  case 2:
+    console.log('오늘은 화요일입니다.');
+    break;
+  case 3:
+    console.log('오늘은 수요일입니다.');
+    break;
+  case 4:
+    console.log('오늘은 목요일입니다.');
+    break;
+  case 5:
+    console.log('오늘은 금요일입니다.');
+    break;
+  case 6:
+    console.log('오늘은 토요일입니다.');
+    break;
+}
+
+// switch문 사용하지 않는 방법
+const WEEK_NAMES = '일월화수목금토';
+console.log(`오늘은 ${WEEK_NAMES[day]}요일입니다.`);
---
+++ js/trythis/52p.js
@@ -0,0 +1,15 @@
+//다음과 같이 올바른 더하기 연산을 하는 addPoints 함수를 작성하시오.
+//(단, 소숫점 자리수는 긴 쪽에 맞춘다)
+
+0.21354 + 0.1; // 0.31354000000000004
+0.14 + 0.28; // 0.42000000000000004
+0.34 + 0.226; // 0.5660000000000001
+function addPoints(a, b) {
+  return (result = (a + b).toFixed(
+    Math.max(a.toString().length, b.toString().length) - 2
+  ));
+}
+
+console.log(addPoints(0.21354, 0.1)); // 0.31354
+console.log(addPoints(0.14, 0.28)); // 0.42
+console.log(addPoints(0.34, 0.226)); // 0.566
