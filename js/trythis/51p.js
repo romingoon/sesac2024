10201a24cb60dda13fc93beb424a14e8aed132d3
romingoon <romingoon@gmail.com>
2024-06-08 00:53:17 +0900

    TryThis 51p - 일부 수정

--- js/trythis/51p.js
+++ js/trythis/51p.js
@@ -2,31 +2,33 @@
 
 // switch문 사용
 const today = new Date().getDay();
-
-switch (today) {
-  case 0:
-    console.log('오늘은 일요일입니다.');
-    break;
-  case 1:
-    console.log('오늘은 월요일입니다.');
-    break;
-  case 2:
-    console.log('오늘은 화요일입니다.');
-    break;
-  case 3:
-    console.log('오늘은 수요일입니다.');
-    break;
-  case 4:
-    console.log('오늘은 목요일입니다.');
-    break;
-  case 5:
-    console.log('오늘은 금요일입니다.');
-    break;
-  case 6:
-    console.log('오늘은 토요일입니다.');
-    break;
+function getDayName(day) {
+  switch (day) {
+    case 0:
+      date = '일요일';
+      break;
+    case 1:
+      date = '월요일';
+      break;
+    case 2:
+      date = '화요일';
+      break;
+    case 3:
+      date = '수요일';
+      break;
+    case 4:
+      date = '목요일';
+      break;
+    case 5:
+      date = '금요일';
+      break;
+    case 6:
+      date = '토요일';
+      break;
+  }
+  return console.log(`오늘은 ${date}입니다.`);
 }
-
+getDayName(today);
 // switch문 사용하지 않는 방법
 const WEEK_NAMES = '일월화수목금토';
 console.log(`오늘은 ${WEEK_NAMES[today]}요일입니다.`);
